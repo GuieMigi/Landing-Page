0bbd7353a23940e1b9fcc8d10e93ca21196e93dc
GuieMigi <florinchiriac2004@yahoo.com>
2020-05-14 12:37:25 +0300

    feat: added navbar items in JS

--- js/script.js
+++ js/script.js
@@ -23,3 +23,23 @@
 // Scroll to section on link click
 
 // Set sections as active
+
+const pageSection = document.querySelectorAll("section");
+const navbar = document.getElementById("navbar-list");
+
+function generateNavbar() {
+    let fragment = document.createDocumentFragment();
+
+    for (let i = 1; i <= pageSection.length; i++){
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<a href="#section${[i]}" style="color: white; text-decoration: none;">Section ${[i]}</a>`;
+        fragment.appendChild(listItem);
+        listItem.style.cssText = "padding: 10px";
+    }
+
+    navbar.appendChild(fragment);
+    navbar.style.backgroundColor = "black";
+}
+
+// Populate the navigation bar
+generateNavbar();

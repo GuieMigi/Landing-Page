f2e1e9b1edb800d640a48b2c8a922d73d23dd39e
GuieMigi <florinchiriac2004@yahoo.com>
2020-05-18 12:00:39 +0300

    feat: added active class on clicked button

--- js/script.js
+++ js/script.js
@@ -24,9 +24,16 @@
 
 // Set sections as active
 
+// Global variables
 const pageSections = document.querySelectorAll("section");
 const navbar = document.getElementById("navbar-list");
-let currentItem = "";
+
+// A helper function that removes the active class from all the buttons
+function removeActiveClass() {
+        document.querySelectorAll("li").forEach(button => {
+        button.classList.remove("active-button");
+    });
+}
 
 // Generates the navbar items based on the available items
 function generateNavbar() {
@@ -34,17 +41,8 @@ function generateNavbar() {
 
     for (let i = 1; i <= pageSections.length; i++){
         const listItem = document.createElement("li");
-        listItem.innerHTML = `<a id="button${[i]}" href="#section${[i]}" style="color: white; text-decoration: none;">Section ${[i]}</a>`;
+        listItem.innerHTML = `<a id="button${i}" class="button" href="#section${i}" style="color: white; text-decoration: none;">Section ${i}</a>`;
         fragment.appendChild(listItem);
-        currentItem = listItem.id;
-        console.log(currentItem);
-        listItem.addEventListener("click", function() {
-            if (currentItem == listItem.id){
-                listItem.classList.add("active-button");
-            } else {
-                listItem.classList.remove("active-button");  
-            }
-        })
     }
 
     navbar.appendChild(fragment);
@@ -53,3 +51,18 @@ function generateNavbar() {
 
 // Populate the navigation bar
 generateNavbar();
+
+// Check that the DOM was generated
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button list and add a listener on the buttons to add or remove the active class
+    document.querySelectorAll("li").forEach(listItem => {
+        listItem.addEventListener('click', () => {
+            removeActiveClass();
+            if (listItem.classList.contains("active-button")) {
+                listItem.classList.remove("active-button");
+            } else {
+                listItem.classList.add("active-button");
+            }
+        });
+    });
+});
